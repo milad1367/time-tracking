326f2ea5619fda31c9f2a96abb886edc44d0d59d
milad asghari <miladasghari2016@gmail.com>
2018-04-16 20:41:26 +0430

    add elapsed state

---
+++ src/components/Tracking.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Tracking extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {elapsed: 0};
+    }
+    render() {
+      return (
+        <div>
+        
+        </div>
+      );
+    }
+  }
+  
+  export default Tracking;
