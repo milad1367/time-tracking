1b1481dd334f3f9598977259a26132b71a6deaff
milad asghari <miladasghari2016@gmail.com>
2018-04-16 21:01:37 +0430

    add tracking

--- src/components/Tracking.js
+++ src/components/Tracking.js
@@ -6,16 +6,23 @@ class Tracking extends Component {
         this.state = {elapsed: 0};
         this.tick = this.tick.bind(this);
     }
+    componentDidMount(){
+       this.timer = setInterval(this.tick, 50);
+    }
+    componentWillUnmount(){
+       clearInterval(this.timer);
+    }
     tick() {
         this.setState({
             elapsed: new Date() - this.props.start
-
         });
     }
     render() {
+      var elapsed = Math.round(this.state.elapsed / 100);
+      var seconds = (elapsed / 10).toFixed(1);    
       return (
         <div>
-        
+          {seconds}
         </div>
       );
     }
